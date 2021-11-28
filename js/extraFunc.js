08a899d14398eefbb091af30a758eed271d11384
Daniel Baumann <d.baumann@baumann-computer.de>
2021-11-28 19:05:10 +0100

    add comments

--- js/extraFunc.js
+++ js/extraFunc.js
@@ -1,33 +1,6 @@
-/*
-Mit Mausklick Start & Ziel definieren mit Reset-Button
-
-An der Seite folgende Merkmale aufgelistet:
-
-Anzahl Felder durchlaufen
-
-Anzahl Kosten durchlaufen
-
-Boot genutzt? Ja / Nein
-
-{EVENTUELL NICHT ERFORDERLICH: Zeilen und Spalten der Felder nummerieren (wie Schachbrett, Excel-Tabelle, ..}
-
-In GRÜN alle Felder NACHEINANDER markieren lassen, welche mitberücksichtigt werden. Anschließend in ROT den tatsächlich gewählten Weg
-
-Eventuell nicht Endergebnis direkt aufzeigen, sondern den gelaufenen Feldweg Schritt für Schritt rot markieren
-
-Button, um grüne Felder nach Abschluss des A*-Algorithmus zu aktivieren/deaktivieren
-
-Das gleiche für roten Pfad
-
-BONUS: Zeitstrahl (als X-Achse) mit Scroll-Funktion, dass man den ganzen Suchverlauf chronologisch sehen kann
-
-Nach Beendigung des Durchlaufens, grüne Felder deaktivieren
-
-
-
-* */
-
-function displayTime(){
+// Für Benutzerfreundlichkeit: Suchgeschwindigkeit
+// Fügt die ausgewählte Suchzeit in den localStorage, um beim neuladen die Informationen zu speichern!
+function setSearchTimeToLocalStorage(){
 
     document.querySelector("#time").addEventListener ("input", function () {
         localStorage.setItem("searchTime", this.value);
@@ -35,9 +8,11 @@ function displayTime(){
 
 }
 
-// Zeigt die Auswahl, ob Start oder Ziel definiert werden soll.
+// Für Auswahl: Start und Ziel
+// Zeigt die Auswahl, ob Start oder Ziel im Gride an
 function displayOption(id, showOption){
     let element = document.getElementById(id);
+    // Wahr, wenn Maus auf Feld geht & // Falsch, wenn Maus das Feld verlässt
     if(showOption){
         if(getStart() == null){
             element.style.backgroundColor = color["startBegin"];
@@ -52,6 +27,7 @@ function displayOption(id, showOption){
         }
 
     }else{
+        // Feld nur ändern, wenn es nicht das Start-, Endfeld oder besuchte Felder (Closedlist) ist
         if(id != getStart() && id != getEnd() && !closedList.includes(id)){
             let type = element.getAttribute("type");
             element.innerHTML = type;
@@ -63,11 +39,13 @@ function displayOption(id, showOption){
 
 }
 
+// Für Auswertung: Mehr Details
+// Berechnet zusätzliche Informationen zu dem gelaufenen Gride
 function showMoreDetails(){
+  // Display gelaufene Felder
+    let current = getEnd(); // Weg wird von Ende zum Start zurück verfolgt
+    let throwBoat = false; // Wurde das Boot auf dem Weg weggeworfen?
 
-    // Display gelaufene Felder
-    let current = getEnd();
-    let throwBoat = false;
     let amount = {
         "all":0,
         "forest":0,
@@ -76,40 +54,46 @@ function showMoreDetails(){
         "way":0,
         "flat":0,
     };
-    let costs = 0;
+    let costs = 0; // Kosten, die bis zum Wegwerfen des Bootes entstanden sind
+
     while(current != getStart()){
         let tmpType = document.getElementById(current).getAttribute("type");
         let tmpThrowBoat = document.getElementById(current).getAttribute("throwBoat");
+
         if(tmpThrowBoat == "true"){
             throwBoat = true;
-            console.log("Bot geschmissen an:");
-            console.log(current);
-            console.log("Wegkosten bis dato:")
             costs = document.getElementById(current).getAttribute("pathCost");
-
         }
+
         amount["all"] = amount["all"] + 1;
         amount[type[tmpType]] = amount[type[tmpType]] + 1;
         current = parents.get(current);
     }
+    // Feldkosten anzeigen lassen
     document.getElementById("runnedFields").innerHTML = amount["all"].toString();
     document.getElementById("runnedFieldsFlat").innerHTML = amount["flat"].toString();
     document.getElementById("runnedFieldsRiver").innerHTML = amount["river"].toString();
     document.getElementById("runnedFieldsForest").innerHTML = amount["forest"].toString();
     document.getElementById("runnedFieldsMountain").innerHTML = amount["mountain"].toString();
     document.getElementById("runnedFieldsWay").innerHTML = amount["way"].toString();
-    //console.log(amount);
-    console.log(costs);
+
+
     // Zeitkosten darstellen
-    let roundFactor = 3;
+    let roundFactor = 3; // Nachkommastelle zu dem die Zeitkosten gerundert werden sollen
+
+    // Gelaufene Zeitkosten
     let runnedCosts = roundFloat(parseFloat(document.getElementById(getEnd()).getAttribute("pathCost")), roundFactor);
     document.getElementById("runnedCosts").innerHTML = runnedCosts;
+
+    // Gelaufene Zeitkosten, wenn Boot direkt zu Anfang weggeworfen wurde
     let lowerRunnedCosts = roundFloat(parseFloat(document.getElementById(getEnd()).getAttribute("pathCost"))-costs +costs*(1-reduze), roundFactor);
     document.getElementById("lowerRunnedCosts").innerHTML = lowerRunnedCosts;
+
     let diff = runnedCosts -lowerRunnedCosts;
     document.getElementById("diffRunnedCosts").innerHTML = roundFloat(diff, roundFactor);
 
 
+    // Wurde ein Fluss überquert
     if(throwBoat == true || document.getElementById(getEnd()).getAttribute("hasBoat") == "true" && document.getElementById(getEnd()).getAttribute("type") != "0"){
         document.getElementById("usedBoat").innerHTML = '<i class="fas fa-times"></i>';
         document.getElementById("usedBoat").setAttribute("style", "color: rgb(255, 73, 73)");
@@ -117,6 +101,8 @@ function showMoreDetails(){
         document.getElementById("usedBoat").innerHTML = '<i class="fas fa-check"></i>';
         document.getElementById("usedBoat").setAttribute("style", "color: lightgreen");
     }
+
+    // Wurde das Boot weggeworfen
     if(throwBoat == true){
         document.getElementById("throwBoat").innerHTML = '<i class="fas fa-check"></i>';
         document.getElementById("throwBoat").setAttribute("style", "color: lightgreen");
