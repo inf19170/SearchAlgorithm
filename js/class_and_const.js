68ad3855fbc4763ba00cc2f3beb27849cbd52f22
inf19170 <91903820+inf19170@users.noreply.github.com>
2021-12-04 17:10:38 +0100

    Update class_and_const.js

--- js/class_and_const.js
+++ js/class_and_const.js
@@ -8,6 +8,6 @@ class Field {
 
     createHTML(x,y){
         const id = x+":"+y;
-           return "<td style='border: solid 0.1px black;background-color:"+this.color+"; width: 20px; height: 10px; font-size: 8px;' cost='"+this.costs+"' id='"+id+"' type='"+this.type+"' hasBoat='"+this.hasBoat+"'  onclick='setStartAndEnd(\""+id+"\");' onmouseover='displayOption(\""+id+"\", true);' onmouseleave='displayOption(\""+id+"\", false);' throwBoat='false'>"+this.type+"</td>"
+           return "<td style='border: solid 0.1px black;background-color:"+this.color+"; width: 20px; height: 10px; font-size: 8.5px;' cost='"+this.costs+"' id='"+id+"' type='"+this.type+"' hasBoat='"+this.hasBoat+"'  onclick='setStartAndEnd(\""+id+"\");' onmouseover='displayOption(\""+id+"\", true);' onmouseleave='displayOption(\""+id+"\", false);' throwBoat='false'>"+this.type+"</td>"
     }
 }
