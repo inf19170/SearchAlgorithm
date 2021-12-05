36bca0e95386243e115fcc517c24aed3d8dc83eb
Daniel Baumann <d.baumann@baumann-computer.de>
2021-12-05 16:15:50 +0100

    refactor

---
+++ js/class/Field.js
@@ -0,0 +1,13 @@
+class Field {
+    constructor(type) {
+        this.type = type;
+        this.costs = cost[type];
+        this.color = color[type];
+        this.hasBoat = true;
+    }
+
+    createHTML(x,y){
+        const id = x+":"+y;
+           return "<td style='border: solid 0.1px black;background-color:"+this.color+"; width: 20px; height: 10px; font-size: 8.5px;' cost='"+this.costs+"' id='"+id+"' type='"+this.type+"' hasBoat='"+this.hasBoat+"'  onclick='setStartAndEnd(\""+id+"\");' onmouseover='displayOption(\""+id+"\", true);' onmouseleave='displayOption(\""+id+"\", false);' throwBoat='false'>"+this.type+"</td>"
+    }
+}
